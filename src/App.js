9a66f42a399e6d95a8ec39519a828bb19977ff68
Donghua Zhang <donghua.zhang@compass.com>
2023-06-28 11:05:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
+// cNfRFo fdjles
 // ejECfw ejECfw
 const OperationButton = styled.div`
   font-size: var(--cx-font-sizeBody1);
   font-weight: var(--cx-font-weightMedium);
   margin-left: var(--cx-spacing-3x);
+  color: blue;
+  content: "xiaoming";
+  border: 2px solid #009ee0;
   ${({ $disabled }) => $disabled &&
           `
     pointer-events: none;
